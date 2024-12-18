70c8e8ed2acbbcf76709e1be61452298b6a9f2ae
Tarantaika-52 <sasha-kvasnikov@mail.ru>
2024-12-18 19:41:29 +0300

    feat: add router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "./assets/vars.css"
+import CommingSoonPage from './react/pages/comming_soon'
+import DebugPage from './react/pages/test'
 
 function App() {
 
-  const bg_array = [
-    'https://i.pinimg.com/originals/11/f3/f3/11f3f352a9fc8c297f02cfc993fe06b3.gif',
-    "https://i.pinimg.com/originals/50/53/5c/50535c6c1c6c76f3719d975a272945c7.gif",
-    "https://i.pinimg.com/originals/d3/5c/cd/d35ccd51f367825006f3534751e87154.gif",
-    "https://i.pinimg.com/originals/66/e8/49/66e8498d24c8e5b03ffd6647cd8e6076.gif"
-  ]
-
-  function getRandomInt(max:Number) {
-    return Math.floor(Math.random() * Number(max));
-  }
-
   return (
-    <>
-      <div className="title">
-        <h1 className='comming_soon_title'>{"Coming soon"}</h1>
-      </div>
-
-      <div className="sub">
-        <a className='subtitle' href='https://github.com/Tarantaika-52/just'>{"github"}</a>
-        <h4 className='subtitle'>{"/"}</h4>
-      </div>
-
-      {/* <img className='background' src='https://i.pinimg.com/originals/11/f3/f3/11f3f352a9fc8c297f02cfc993fe06b3.gif'/> */}
-      <img className='background' src={bg_array[getRandomInt(bg_array.length)]}/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<CommingSoonPage/>}/>
+        <Route path='/test' element={<DebugPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/react/pages/test.tsx
@@ -0,0 +1,22 @@
+function DebugPage(){
+
+    return(
+        <>
+            <div className="title">
+            <h1 className='comming_soon_title'>{"It's just a test page"}</h1>
+            </div>
+
+            <div className="sub">
+              <a className='subtitle' href='https://github.com/Tarantaika-52/just'>{"github"}</a>
+              <h4 className='subtitle'>{"/"}</h4>
+              <h4 className='subtitle'>{"80d8e5"}</h4>
+              <h4 className='subtitle'>{"/"}</h4>
+              <a className='subtitle' href="..">{"at home"}</a>
+            </div>
+
+            <img className='background' src="https://i.pinimg.com/736x/7d/26/55/7d26557b7c3a1b202b29d4d517106818.jpg"/>
+        </>
+    )
+}
+
+export default DebugPage;
